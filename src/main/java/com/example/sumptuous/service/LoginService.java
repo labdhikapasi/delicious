d6ccf2991be8c0b7d43a9c1f210f46e35bfd8e11
labdhi <labdhik2@gmail.com>
2021-05-19 03:39:27 +0530

    added USER role in register api

--- src/main/java/com/example/sumptuous/service/LoginService.java
+++ src/main/java/com/example/sumptuous/service/LoginService.java
@@ -3,6 +3,7 @@ package com.example.sumptuous.service;
 import com.example.sumptuous.bean.User;
 import com.example.sumptuous.controller.LoginController;
 import com.example.sumptuous.dao.UserRepository;
+import com.example.sumptuous.enums.Role;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,7 @@ public class LoginService {
 
     public User registerUser(User user){
         logger.info("[registerUser] argument : user emailId : "+user.getEmailId());
+        user.setRole(Role.USER);
         User user1 = userRepository.save(user);
         logger.info("[registerUser] response : user1 id : "+user1.getId());
         return user1;
